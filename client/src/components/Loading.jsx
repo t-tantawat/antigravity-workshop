59fcc15b4acdf5976dcfbb529434abb81423c8ee
Thongchai Tantawat <thongchai.dev@gmail.com>
2025-11-24 16:13:56 +0700

    update4

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className="loading-container">
+            <div className="loading-spinner"></div>
+            <p>กำลังโหลดข้อมูล...</p>
+        </div>
+    );
+};
+
+export default Loading;
